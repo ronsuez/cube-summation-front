afbe7994c2971023a155ae2cc0f7d35cbd0c8839
Ronald Suez <rsuez93@gmail.com>
2017-04-04 01:00:14 -0400

    finishing last step

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {AppService} from './app.service';
 import {Observable} from "rxjs/Rx";
+import {map} from 'async-es/map';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,32 @@ export class AppComponent {
   public title : string;
   public commands : Array<Object> = [];
   public command : string;
+
+  private testCases = [
+    {
+      dimension: 4,
+      operations: 5,
+      init: '/create-cube 4',
+      commands: [
+        '/update-cube -cubeid- 2 2 2 4',
+        '/query-cube -cubeid- 1 1 1 3 3 3',
+        '/update-cube -cubeid- 1 1 1 23',
+        '/query-cube -cubeid- 2 2 2 4 4 4',
+        '/query-cube -cubeid- 1 1 1 3 3 3'
+      ]
+    },
+      {
+      dimension: 2,
+      operations: 4,
+      init: '/create-cube 2',
+      commands: [
+        '/update-cube -cubeid- 2 2 2 1',
+        '/query-cube -cubeid- 1 1 1 1 1 1',
+        '/query-cube -cubeid- 2 2 2 2 2 2',
+        '/query-cube -cubeid- 2 2 2 2 2 2'
+      ]
+    }
+  ]
 
   constructor (private appService: AppService) {
     this.title = 'Cube cli v1'
@@ -32,6 +59,7 @@ export class AppComponent {
             * /update-cube [cube-id] [x y z value]
             * /query-cube [cube-id] [x1 y1 z1 x2 y2 z2]
             * /list-cubes
+            * /run-test-cases [1,2]
 
         `
     })
@@ -46,32 +74,14 @@ export class AppComponent {
 
     this.command = ''
 
-    this.commands.push(expression);
+    console.log(expression);
 
-    this.runCommand(expression).then((resp) => {
-      console.log(resp)
-    }).catch((err) => console.log(err))
-
-  }
-
-  helpCommand() {
-    return {
-        id: 0,
-        action: 'help',
-        params: [],
-        command: `
-
-          Welcome to the Cube-Cli... \n
-
-          This is a simple command line tool intended to run test cases to the Cube API.
-
-          ### Commands:
-            * /create-cube [dimension]
-            * /update-cube [cube-id] [x y z value]
-            * /query-cube [cube-id] [x1 y1 z1 x2 y2 z2]
-            * /list-cubes
-
-        `
+    if (expression.action === 'runTestCases') {
+      this.runTestCases(expression);
+    } else {
+      this.runCommand(expression)
+        .then((resp) => this.serverCommand(expression, resp))
+        .catch((err) => console.log(err))
     }
   }
 
@@ -79,13 +89,12 @@ export class AppComponent {
     return Object.assign(this.parseCommand(command), {
       id: 1,
       command: command,
-      date: new Date()
+      date: new Date(),
+      isClient: true
     });
   }
 
   parseCommand(command) {
-
-    console.log(command);
 
     let params = command.split(" ").filter((a) => a.length);
 
@@ -93,10 +102,10 @@ export class AppComponent {
       '/create-cube': 'createCube',
       '/update-cube': 'updateCube',
       '/query-cube': 'queryCube',
-      '/list-cubes': 'getCubes'
+      '/list-cubes': 'getCubes',
+      '/run-test-cases' : 'runTestCases'
     };
 
-    console.log(params);
 
     if (options.hasOwnProperty(params[0])) {
       return {
@@ -110,16 +119,92 @@ export class AppComponent {
   }
 
   runCommand(command) {
+
     console.log(command);
 
-    if(!command || !this.appService[command.action]) return Promise.reject('command not available');
+    this.commands.push(command);
 
-    return this.appService[command.action](command.params).subscribe(
-      (resp) => Promise.resolve(resp),
-      (err) => Promise.reject('err'),
-      () => console.log('completed')
-      );
+    if (!command) return Promise.reject('command not available on cli');
+
+    if(!this.appService[command.action]) return Promise.reject('command not available on service');
+
+    return this.appService[command.action](command.params);
+  }
+
+
+  serverCommand(command, payload) {
+
+    let options = {
+      'createCube': (data) => `Cube created with ID: ${data._id}`,
+      'updateCube': (data) => `Cube with ID: ${data._id} at position(${data.position}) updated with value ${data.value}`,
+      'queryCube': (data) => `Cube with ID: ${data._id} at position(${data.position}) has value ${data.value}`,
+      'getCubes': (data) => `there are ${data.length} cubes`,
+      'runTestCases': (data) => 'Running test cases'
+    };
+
+    let errors = {
+      'updateCube': (data) => `Could not find cube with ID: ${data._id}`,
+      'queryCube': (data) => `Could not find cube with ID: ${data._id}`
+    }
+
+    this.commands.push({
+      id: 1,
+      command: command,
+      date: new Date(),
+      isServer: true,
+      payload: payload.err ? payload.err : options[command.action](payload.data)
+    });
 
   }
+
+
+  renderCommand(message) {
+    if (message.isServer) {
+      return `$ server:  ${message.payload}`;
+    }
+
+    if(message.isClient) {
+      return `$ me:  ${message.command}`;
+    }
+
+    return `$ system: ${message.command}`;
+  }
+
+
+  runTestCases(expression) {
+
+    console.log('should run test cases', expression);
+
+    let test = parseInt(expression.params[0]);
+
+    if(test > 2) return;
+
+    this.serverCommand(expression, {err: null, data: {}});
+
+
+    this.runTestCommand(this.testCases[test - 1]);
+
+  }
+
+  runTestCommand(set) {
+    let expression = this.createCommand(set.init);
+
+    this.runCommand(expression)
+    .then((resp) => {
+
+      this.serverCommand(expression, resp);
+
+      let commands = set.commands.map((command => command.replace('-cubeid-', resp.data._id)));
+
+      console.log(commands);
+
+      commands.map((command) => {
+        let expression = this.createCommand(command);
+        this.runCommand(expression)
+            .then(resp => this.serverCommand(expression, resp));
+      })
+    })
+  }
+
 
 }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -3,6 +3,11 @@ import {Http, Headers, RequestOptions, Response} from '@angular/http';
 
 import { Observable } from 'rxjs/Rx';
 
+// Import RxJs required methods
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/toPromise';
+
 @Injectable()
 export class AppService {
 
@@ -14,16 +19,48 @@ export class AppService {
     let headers = new Headers({ 'Content-Type': 'application/json' });
     let options = new RequestOptions({ headers: headers });
     return this.http.post(this.base, params, options)
-      .map((resp) => resp.json())
-      .catch((err) => Promise.reject(err))
+    .toPromise()
+    .then((resp) => resp.json())
+    .catch((err) => console.log(err));
   }
 
   getCubes(params) {
     let headers = new Headers({ 'Content-Type': 'application/json' });
     let options = new RequestOptions({ headers: headers });
     return this.http.get(this.base, options)
-      .map((resp) => resp.json())
-      .catch((err) => Promise.reject(err))
+    .toPromise()
+    .then((resp) => resp.json())
+    .catch((err) => console.log(err) );
+  }
+
+  updateCube(params) {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    return this.http.post(
+      [this.base, params[0], 'update'].join('/'),
+      {
+        position: params.slice(1, params.length - 1),
+        value: params.slice(4, params.length)
+      },
+      options)
+    .toPromise()
+    .then((resp) => resp.json())
+    .catch((err) => console.log(err) );
+  }
+
+
+  queryCube(params) {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    return this.http.post(
+      [this.base, params[0], 'query'].join('/'),
+      {
+        position: params.slice(1, params.length),
+      },
+      options)
+    .toPromise()
+    .then((resp) => resp.json())
+    .catch((err) => console.log(err) );
   }
 
 
